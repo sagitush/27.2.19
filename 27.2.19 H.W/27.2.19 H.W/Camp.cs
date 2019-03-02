68565c9279646c70e7e8745fd71006ca1581e43f
sagitush <44786027+sagitush@users.noreply.github.com>
2019-03-02 20:14:07 +0200

    Add files via upload

---
+++ 27.2.19 H.W/27.2.19 H.W/Camp.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._2._19_H.W
+{
+   public class Camp
+    {
+        public int id { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get;  set; }
+        public int NumberOfPeople { get;  set; }
+        public int NumberOfTents { get;  set; }
+        public int NumberOfFleshLights { get;  set; }
+        private static int lastCampld = 0;
+
+        public Camp(int latitude, int longitude, int numberOfPeople, int numberOfTents, int numberOfFleshLights)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            NumberOfPeople = numberOfPeople;
+            NumberOfTents = numberOfTents;
+            NumberOfFleshLights = numberOfFleshLights;
+            Camp.lastCampld++;
+            this.id = Camp.lastCampld;
+        }
+
+        public override string ToString()
+        {
+            return $"Camp ID:{this.id,5}  Latitude{Latitude,5}  longitude{Longitude,5}  number of people:{NumberOfPeople,5}  number of tents{NumberOfTents,5}  number of flesh lights{NumberOfFleshLights}";
+        }
+
+        public static bool operator ==(Camp camp1,Camp camp2)
+        {
+            if (camp1.id == camp2.id)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool operator !=(Camp camp1, Camp camp2)
+        {
+            return !(camp1.id == camp2.id);
+        }
+        public static bool operator >(Camp camp1, Camp camp2)
+        {
+            if (camp1.NumberOfPeople > camp2.NumberOfPeople)
+            {
+                return true;
+            }
+            else if (camp1.NumberOfPeople == camp2.NumberOfPeople)
+            {
+                Console.WriteLine("the same");
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool operator <(Camp camp1, Camp camp2)
+        {
+            return !(camp1.NumberOfPeople > camp2.NumberOfPeople);
+        }
+        public override bool Equals(object obj)
+        {
+            Camp otherCamp = obj as Camp;
+            return this == otherCamp;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.id;
+        }
+        public static Camp operator +(Camp camp1, Camp camp2)
+        {
+            return new Camp(camp1.Latitude + camp2.Latitude, camp1.Longitude + camp2.Longitude, camp1.NumberOfPeople + camp2.NumberOfPeople, camp1.NumberOfTents + camp2.NumberOfTents, camp1.NumberOfFleshLights + camp2.NumberOfFleshLights);
+            
+        }
+        public Camp()
+        {
+                
+        }
+    }
+}
---
+++ 27.2.19 H.W/27.2.19 H.W/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _27._2._19_H.W
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camp camp1 = new Camp(40, 50, 50, 25, 50);
+            Camp camp2 = new Camp(70, 60, 65, 33, 65);
+            /*if(camp1.NumberOfPeople>camp2.NumberOfPeople)
+            {
+                Console.WriteLine("big");
+            }
+            else if(camp1.NumberOfPeople==camp2.NumberOfPeople)
+            {
+                Console.WriteLine("the same");
+            }
+            else
+                Console.WriteLine("small");
+            Console.WriteLine(camp1 > camp2);
+           Camp camp3= camp1 + camp2;
+            Console.WriteLine(camp3);
+          
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Camp));
+
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Create)) // creating new file stream
+            {
+                myXmlSerializer.Serialize(file, camp1);
+
+            } //closing file stream
+            */
+
+            Camp p2;
+
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Camp));
+
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Open)) // creating new file stream
+            {
+                p2 = myXmlSerializer.Deserialize(file) as Camp;
+
+            }
+            Camp camp;
+
+            XmlSerializer XmlSerializer = new XmlSerializer(typeof(Camp));
+
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Open)) // creating new file stream
+            {
+                camp = XmlSerializer.Deserialize(file) as Camp;
+
+            }
+            Console.WriteLine(camp==p2);
+            Console.WriteLine( camp.GetHashCode());
+            Console.WriteLine(p2.GetHashCode());
+
+        }
+    }
+}
